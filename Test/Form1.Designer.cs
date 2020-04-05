aea63b3c80b0db48a193f37ff29c492a9540306c
PHV1710303 <63140813+PHV1710303@users.noreply.github.com>
2020-04-05 18:49:06 +0700

    Test
    Test thử!!

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn = new System.Windows.Forms.Button();
+            this.rickTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(12, 12);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(75, 23);
+            this.btn.TabIndex = 0;
+            this.btn.Text = "Button";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // rickTextBox
+            // 
+            this.rickTextBox.Location = new System.Drawing.Point(117, 12);
+            this.rickTextBox.Name = "rickTextBox";
+            this.rickTextBox.Size = new System.Drawing.Size(921, 646);
+            this.rickTextBox.TabIndex = 1;
+            this.rickTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1050, 670);
+            this.Controls.Add(this.rickTextBox);
+            this.Controls.Add(this.btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.RichTextBox rickTextBox;
+    }
+}
+
---
+++ Test/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static String execCommand(String filename, string arguments)
+        {
+            StreamReader outputReader = null;
+            StreamReader errorReader = null;
+
+            String result = "";
+
+            try
+            {
+                //Create Process Start information
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.ErrorDialog = false;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.RedirectStandardError = true;
+                processStartInfo.RedirectStandardInput = true;
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.CreateNoWindow = false;
+
+                processStartInfo.FileName = filename;
+                processStartInfo.Arguments = arguments;
+                processStartInfo.Verb = "runas"; // run as Administrator.
+
+                //Execute the process
+                Process process = new Process();
+                process.StartInfo = processStartInfo;
+                bool processStarted = process.Start();
+                if (processStarted)
+                {
+                    //Get the output stream
+                    outputReader = process.StandardOutput;
+                    errorReader = process.StandardError;
+                    process.WaitForExit();
+
+                    //read the result
+
+                    result = outputReader.ReadToEnd();
+
+                    //Display the result
+
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+                //MessageBox.Show(ex.Message);
+                return result;
+            }
+            finally
+            {
+                if (outputReader != null)
+                {
+                    outputReader.Close();
+                }
+                if (errorReader != null)
+                {
+                    errorReader.Close();
+                }
+            }
+        }
+
+        private IEnumerable<DirectoryInfo> LayDanhSachThuMuc(IEnumerable<string> dsDuongDan)
+        {
+            return dsDuongDan
+                .Select(duongDan => new DirectoryInfo(duongDan))
+                .SelectMany(thuMuc => thuMuc.GetDirectories())
+                .ToList();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string str = Environment.ExpandEnvironmentVariables("%SystemRoot%") + @"\System32\cmd.exe";
+            //execCommand("cmd.exe", "/K net user Zoro /active:yes");
+            rickTextBox.Text = execCommand("cmd.exe", "/C ipconfig");
+        }
+    }
+}
